e6234ef6c106ba9bcb7a93103f0feb357959834d
Alexey Rudoy <rudenw01@gmail.com>
2022-03-01 17:00:16 +0300

    feat: DI through setter

---
+++ src/main/java/com/rudoy/project/FolkMusic.java
@@ -0,0 +1,8 @@
+package com.rudoy.project;
+
+public class FolkMusic implements Music {
+    @Override
+    public String getSong() {
+        return "Love, brothers, love";
+    }
+}
--- src/main/java/com/rudoy/project/MusicPlayer.java
+++ src/main/java/com/rudoy/project/MusicPlayer.java
@@ -1,14 +1,39 @@
 package com.rudoy.project;
 
+import java.util.List;
+
 public class MusicPlayer {
-    private Music music;
+    private List<Music> musicList;
+    private String name;
+    private int volume;
+
+    public MusicPlayer() {
+    }
 
     // IoC
-    public MusicPlayer(Music music) {
-        this.music = music;
+    public void setMusic(List<Music> musicList) {
+        this.musicList = musicList;
     }
 
     public void playMusic() {
-        System.out.println("Playing: " + music.getSong());
+        for (Music music : musicList) {
+            System.out.println("Playing: " + music.getSong());
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
     }
 }
